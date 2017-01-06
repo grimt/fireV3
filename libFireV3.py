a366e89f62452398fb73ab26547dea4b3df84bf4
grimt <graeme.john.thomson@gmail.com>
2017-01-07 10:54:34 +1300

    Create libFireV3.py

---
+++ libFireV3.py
@@ -0,0 +1,15 @@
+! Library for functionality common accross all modules.
+
+! Data files functionality
+
+Def writeDataToFile (filename, data)
+! filename:   string - full path to file 
+! data:       string - data to write
+! exceptions: log and continue
+  try:
+        f = open (filename, 'wt')
+        f.write (data)
+        f.close ()
+
+  except IOError:
+my_logger.exception ("Cant open file fire_status.txt for writing")
