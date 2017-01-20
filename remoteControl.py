bdf9ad46ec843fb368261ed115298f415fcc640d
grimt <graeme.john.thomson@gmail.com>
2017-01-20 16:13:52 +1300

    Create file

---
+++ remoteControl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Read from remote control via flirc
+
+# modules to read from the flirc
+from evdev import InputDevice, categorize, ecodes
+
+dev = InputDevice ('/dev/input/event0')
+
+for event in dev.read_loop():
+    #
+    # type should always be 1 for a keypress
+    # code is the numeric value of the key that has been pressed
+    # value 0 = key up, 1 = key down, 2 = key hold
+
+    if event.type == ecodes.EV_KEY:
+        # my_logger.debug (categorize(event))
+        print ( 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value))
+        if event.value == 0:  # key up
+                print ("code: " + event.code)
