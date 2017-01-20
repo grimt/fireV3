17171ba9895461ad21dc002f05a5390793428d43
grimt <graeme.john.thomson@gmail.com>
2017-01-21 08:11:45 +1300

    Put remote control code into a function

--- remoteControl.py
+++ remoteControl.py
@@ -11,15 +11,21 @@
 from evdev import InputDevice, categorize, ecodes
 
 dev = InputDevice ('/dev/input/event0')
+def startRemoteScanning():
+    for event in dev.read_loop():
+        #
+        # type should always be 1 for a keypress
+        # code is the numeric value of the key that has been pressed
+        # value 0 = key up, 1 = key down, 2 = key hold
 
-for event in dev.read_loop():
-    #
-    # type should always be 1 for a keypress
-    # code is the numeric value of the key that has been pressed
-    # value 0 = key up, 1 = key down, 2 = key hold
+        if event.type == ecodes.EV_KEY:
+            # my_logger.debug (categorize(event))
+            print ( 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value))
+            if event.value == 0:  # key up
+                    print ("code: " + str(event.code))
 
-    if event.type == ecodes.EV_KEY:
-        # my_logger.debug (categorize(event))
-        print ( 'type: ' + str (event.type) + ' code: ' + str (event.code) + ' value ' + str (event.value))
-        if event.value == 0:  # key up
-                print ("code: " + str(event.code))
+# Next - put me in my own thread and call me from controlFire
+
+if __name__ == "__main__":
+    print "Executing remoteControl.py test code"
+    startRemoteScanning()
