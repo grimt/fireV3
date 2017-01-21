52053958489559ffc41bed0f18d6f8066e76fe1e
grimt <graeme.john.thomson@gmail.com>
2017-01-22 12:36:04 +1300

    Implement fire state
    The actual state of the fire. Either off or on.

--- controlFire.py
+++ controlFire.py
@@ -11,18 +11,48 @@ from lib.libFile import readDataFromFile
 from lib.libLog import initLogging
 from remoteControl import startRemoteScanning
 
+FIRE_ON = 1
+FIRE_OFF = 0
+
 class cFire:
     def __init__(self):
         self.desiredTemperature = 0
-        self.measuredTemperature = 0
-        self.controlStatus = "off" # manual or automatic
-        self.timeOverride = "off"
+        self.measuredTemperature = 0.0
+        self.controlStatus = "OFF" # Strings - OFF/ON/AUTO
+        self.timeOverride = "OFF"  # Strings OFF/ON
+        self.fireState = FIRE_OFF  # Integer FIRE_OFF/FIRE_ON
+
+    def switchFireOff(self):
+        if self.fireState == FIRE_ON:
+            self.fireState == FIRE_OFF
+            # Switch off fire at relay
+
+    def switchFireOn(self):
+        if self.fireState == FIRE_OFF:
+            self.fireState == FIRE_ON
+            # Switch on fire at relay
+            
+    def runAutoControlAlgorithm (self):
+        pass
 
     def printDebug(self):
+        if self.fireState == FIRE_ON:
+            print "Fire is on"
+        else:
+            print "Fire is off"
+
         print "Desired temperature : " + str(self.desiredTemperature)
         print "Measured temperature: " + str(self.measuredTemperature)
         print "Control Status      : " + self.controlStatus
         print "Time Override       : " + self.timeOverride
+
+    def runContolAlgorith(self):
+        if self.controlStatus == 'OFF':
+            self.switchFireOff()
+        elif self.controlStatus == 'ON':
+            self.switchFireOn()
+        elif self.controlStatus == 'AUTO':
+            self.runAutoControlAlgorithm()
 
 
 fire = cFire()
@@ -48,7 +78,7 @@ try:
         fire.controlStatus = readDataFromFile('datafiles/controlStatus.txt')
         fire.timeOverride = readDataFromFile('datafiles/timeOverride.txt')
 
-        # Next: Run the control algorithm
+        fire.runControlAlgorithm()
 
         time.sleep(1)
 # TODO - make this exception more specific
