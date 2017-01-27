7c31ddbd6f70badf22a3ac9723a384bc7b669773
grimt <graeme.john.thomson@gmail.com>
2017-01-28 12:25:20 +1300

    Add Logging to temp sensor

--- tempSensor.py
+++ tempSensor.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python   
+#!/usr/bin/env python
 #
 # Adadpted from Michael Saunby. April 2013
-# 
+#
 import pexpect
 import sys
 import time
+
+from lib.libLog import initLogging
+
 
 def floatfromhex(h):
     t = float.fromhex(h)
@@ -15,10 +18,11 @@ def floatfromhex(h):
 
 
 def calcTemp(rTemp):
-    return rTemp / 4.0 * 0.03125 
+    return rTemp / 4.0 * 0.03125
 
+tempSensorLogger = initLogging('/var/log/fireV3/tempSensor.log')
 
-bluetooth_adr = 'A0:E6:F8:AF:3C:06' 
+bluetooth_adr = 'A0:E6:F8:AF:3C:06'
 tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
 tool.expect('\[LE\]>')
 print "Preparing to connect. You might need to press the side button..."
@@ -32,7 +36,7 @@ while True:
     time.sleep(1)
     # Read the temp/humidity data
     tool.sendline('char-read-hnd 0x0024')
-    tool.expect('descriptor: .*') 
+    tool.expect('descriptor: .*')
     rVal = tool.after.split()
     #print "Raw data : " + str(rVal)
     rObjTemp = floatfromhex(rVal[2] + rVal[1])
@@ -40,9 +44,9 @@ while True:
 
     rAmbTemp = floatfromhex(rVal[4] + rVal[3])
     # print "Humidity in decimal: " + str (rAmbTemp)
-    
+
     objTemp = calcTemp (rObjTemp)
     ambTemp = calcTemp (rAmbTemp)
 
-    print "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%"
-
+    # print "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%"
+    controlFireLogger.debug ( "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%")
