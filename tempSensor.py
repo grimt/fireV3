e1bee8ff8a92642632b0dabe41dc289bdedbe045
grimtPi3 <graeme.john.thomson@gmail.com>
2017-01-27 23:19:35 +0000

    Add temp sensor

---
+++ tempSensor.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python   
+#
+# Adadpted from Michael Saunby. April 2013
+# 
+import pexpect
+import sys
+import time
+
+def floatfromhex(h):
+    t = float.fromhex(h)
+    if t > float.fromhex('7FFF'):
+        t = -(float.fromhex('FFFF') - t)
+        pass
+    return t
+
+
+def calcTemp(rTemp):
+    return rTemp / 4.0 * 0.03125 
+
+
+bluetooth_adr = 'A0:E6:F8:AF:3C:06' 
+tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
+tool.expect('\[LE\]>')
+print "Preparing to connect. You might need to press the side button..."
+tool.sendline('connect')
+# test for success of connect
+tool.expect('Connection successful')
+# Switch on IR Temp sensor
+tool.sendline('char-write-cmd 0x0027 01')
+tool.expect('\[LE\]>')
+while True:
+    time.sleep(1)
+    # Read the temp/humidity data
+    tool.sendline('char-read-hnd 0x0024')
+    tool.expect('descriptor: .*') 
+    rVal = tool.after.split()
+    #print "Raw data : " + str(rVal)
+    rObjTemp = floatfromhex(rVal[2] + rVal[1])
+    #print "Raw obj temp in decimal: " + str (rObjTemp)
+
+    rAmbTemp = floatfromhex(rVal[4] + rVal[3])
+    # print "Humidity in decimal: " + str (rAmbTemp)
+    
+    objTemp = calcTemp (rObjTemp)
+    ambTemp = calcTemp (rAmbTemp)
+
+    print "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%"
+
