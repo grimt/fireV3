8b85f55cb92d57fc09bd23d410e4e700349d1c3d
grimtPi3 <graeme.john.thomson@gmail.com>
2017-01-28 15:29:28 +1300

    Get the temp sensor working

--- tempSensor.py
+++ tempSensor.py
@@ -22,17 +22,23 @@ def calcTemp(rTemp):
 
 tempSensorLogger = initLogging('/var/log/fireV3/tempSensor.log')
 
-bluetooth_adr = 'A0:E6:F8:AF:3C:06'
+bluetooth_adr = sys.argv[1] # 'A0:E6:F8:AF:3C:06'
+print "connecting to " + sys.argv[1]
+
 tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
 tool.expect('\[LE\]>')
 print "Preparing to connect. You might need to press the side button..."
 tool.sendline('connect')
 # test for success of connect
 tool.expect('Connection successful')
+print "Connection successful"
 # Switch on IR Temp sensor
-tool.sendline('char-write-cmd 0x0027 01')
+#tool.sendline('char-write-cmd 0x0027 01')
 tool.expect('\[LE\]>')
 while True:
+    # time.sleep(5)
+    # Switch on IR Temp sensor
+    tool.sendline('char-write-cmd 0x0027 01')
     time.sleep(1)
     # Read the temp/humidity data
     tool.sendline('char-read-hnd 0x0024')
@@ -49,4 +55,7 @@ while True:
     ambTemp = calcTemp (rAmbTemp)
 
     # print "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%"
-    controlFireLogger.debug ( "Obj: " + "%.2f C" % objTemp + " Amb:  " + "%.2f " % ambTemp + "%")
+    tempSensorLogger.debug ( "Obj: " + "%.2fC" % objTemp + " Amb:  " + "%.2fC" % ambTemp)
+    # Switch off the temp sensor
+    tool.sendline('char-write-cmd 0x0027 00')
+    time.sleep(3)
