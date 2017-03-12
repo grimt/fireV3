a4c2b354ac8be756fd87e413b9a2c39f02b93684
grimt <graeme.john.thomson@gmail.com>
2017-03-12 15:17:48 +1300

    write debug to a file

--- controlFire.py
+++ controlFire.py
@@ -60,26 +60,55 @@ class cFire:
             controlFireLogger.exception ('ValueError exception' + str (self.measuredTemperature))
 
     def printDebug(self):
-        if self.fireState == FIRE_ON:
-            print "Fire is on"
-        else:
-            print "Fire is off"
+        try:
+            f = open ("debug.txt", 'wt')
+            if self.fireState == FIRE_ON:
+                print "Fire is on"
+                f.write("Fire is on")
+            else:
+                print "Fire is off"
+                f.write("Fire is off")
 
-        print "Desired temperature : " + str(self.desiredTemperature)
-        print "Measured temperature: " + str(self.measuredTemperature)
-        print "Control Status      : " + self.controlStatus
-        print "Time Override       : " + self.timeOverride
+            print "Desired temperature : " + str(self.desiredTemperature)
+            f.write("Desired temperature : " + str(self.desiredTemperature))
 
-        print "=============Datafiles==============="
+            print "Measured temperature: " + str(self.measuredTemperature)
+            f.write( "Measured temperature: " + str(self.measuredTemperature))
 
-        print "Control Status: " + readData('datafiles/controlStatus.txt')
-        print "Desired temperature: " + readData('datafiles/desiredTemperature.txt')
-        print "Measured Temperature: " + readData('datafiles/measuredTemperature.txt')
-        print "showStatus: " + readData('datafiles/showStatus.txt')
-        print "systemStatus: " + readData('datafiles/systemStatus.txt')
-        print "timeOverride: " + readData('datafiles/timeOverride.txt')
-        print "LED Brightness: " + readData('datafiles/alphaNumBrightness.txt')
-        print "Override count: " + readData('datafiles/overrideCount.txt.txt')
+            print "Control Status      : " + self.controlStatus
+            f.write "Control Status      : " + self.controlStatus
+
+            print "Time Override       : " + self.timeOverride
+            f.write("Time Override       : " + self.timeOverride)
+
+            print "=============Datafiles==============="
+            f.write("=============Datafiles===============")
+
+            print "Control Status: " + readData('datafiles/controlStatus.txt')
+            f.write "Control Status: " + readData('datafiles/controlStatus.txt')
+
+            print "Desired temperature: " + readData('datafiles/desiredTemperature.txt')
+            f.write "Desired temperature: " + readData('datafiles/desiredTemperature.txt')
+
+            print "Measured Temperature: " + readData('datafiles/measuredTemperature.txt')
+            f.write "Measured Temperature: " + readData('datafiles/measuredTemperature.txt')
+
+            print "showStatus: " + readData('datafiles/showStatus.txt')
+            f.write "showStatus: " + readData('datafiles/showStatus.txt')
+
+            print "systemStatus: " + readData('datafiles/systemStatus.txt')
+            f.write "systemStatus: " + readData('datafiles/systemStatus.txt')
+
+            print "timeOverride: " + readData('datafiles/timeOverride.txt')
+            f.write "timeOverride: " + readData('datafiles/timeOverride.txt')
+
+            print "LED Brightness: " + readData('datafiles/alphaNumBrightness.txt')
+            f.write "LED Brightness: " + readData('datafiles/alphaNumBrightness.txt')
+
+            print "Override count: " + readData('datafiles/overrideCount.txt.txt')
+            f.write "Override count: " + readData('datafiles/overrideCount.txt.txt')
+            f.close ()
+
 
 
 
@@ -124,7 +153,7 @@ showMessageThread.start()
 #TODO - Consider moving all logs and data files to a USB drive
 writeData ('datafiles/showStatus.txt', "BLANK") # Default to nothing on the alphanumeric
 writeData ('datafiles/systemStatus.txt', "GOOD") #BTEr Batt etc. show any errors
-writeData ('datafiles/alphaNumBrightness.txt', "0") # 0 to 15 
+writeData ('datafiles/alphaNumBrightness.txt', "0") # 0 to 15
 writeData ('datafiles/overrideCount.txt', "30") # Check if we need to do a time override every 30 minutes
 
 try:
